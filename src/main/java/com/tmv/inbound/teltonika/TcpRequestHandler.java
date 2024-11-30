7093612a094f4bfa4d8f64d4f9005747b0af1a48
thkrebs <thkrebs@gmail.com>
2024-11-30 21:25:39 +0100

    remove unnecessary imports

--- src/main/java/com/tmv/inbound/teltonika/TcpRequestHandler.java
+++ src/main/java/com/tmv/inbound/teltonika/TcpRequestHandler.java
@@ -4,7 +4,6 @@ import com.tmv.inbound.RequestHandler;
 import com.tmv.inbound.teltonika.model.TcpDataPacket;
 import jakarta.transaction.NotSupportedException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.io.*;
