4126309f7207e4de70972c4751602eba311f7979
thkrebs <thkrebs@gmail.com>
2025-04-02 21:16:04 +0200

    Update spatial query to use transformed coordinates
    Refactor the `findWithinDistance` query to transform both points to the 3857 coordinate system for accurate distance calculations. This ensures proper handling of spatial data and improves query reliability.

--- src/main/java/com/tmv/core/persistence/ParkSpotRepository.java
+++ src/main/java/com/tmv/core/persistence/ParkSpotRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface ParkSpotRepository  extends JpaRepository<ParkSpot, Long>  {
-    @Query(value = "SELECT * FROM parkspot WHERE ST_DWithin(point, ST_SetSRID(ST_MakePoint(?1, ?2), 4326), ?3)", nativeQuery = true)
+    @Query(value = "SELECT * FROM parkspot WHERE ST_DWithin(ST_Transform(point, 3857), ST_Transform(ST_SetSRID(ST_MakePoint(?1, ?2), 4326),3857), ?3)", nativeQuery = true)
     List<ParkSpot> findWithinDistance(double longitude, double latitude, double distanceInMeters);
 
 }
