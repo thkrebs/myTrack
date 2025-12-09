c1ab57fec99b480f24ea90866a8665cb69417cd8
thkrebs <thkrebs@gmail.com>
2025-12-09 15:38:58 +0100

    Test: Pushing a single clean commit

--- src/main/java/com/tmv/core/config/CoreConfiguration.java
+++ src/main/java/com/tmv/core/config/CoreConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//
+
 @org.springframework.context.annotation.Configuration
 public class CoreConfiguration implements WebMvcConfigurer {
 
